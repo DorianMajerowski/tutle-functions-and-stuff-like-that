7cdafda24ed1d03deef2fa452e6e3cdeb6d6df75
Dorian Kajetan Majerowski <cldm096@cl.glow.scot>
2020-09-04 09:20:07 +0000

    circle!

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
 import turtle as t
 t.speed(0)
 
-#main function
 def main():
-	drawSqu(-125, 125, 250)
+	for x in range(360):
+		t.forward(2)
+		t.right(1)
 
-	drawRect(-100, 0, 75, 125)
 
-
-	drawSqu(10, 10, 50)
-	drawSqu(60, 10, 50)
-	drawSqu(10, -40, 50)
-	drawSqu(60, -40, 50)
-
-	drawIsoTri(-125, 125, 250)
-
-#draws rectngle
-def drawRect(x, y, lenX, lenY):
-	t.penup()
-	t.goto(x, y)
-	t.pendown()
-	for i in range(2):
-		t.forward(lenX)
-		t.right(90)
-		t.forward(lenY)
-		t.right(90)
-
-#draws square using drawRect
-def drawSqu(x, y, leng):
-	drawRect(x, y, leng, leng)
-
-#draws isosceles triangle
-def drawIsoTri(x, y, leng):
-	t.penup()
-	t.goto(x, y)
-	t.pendown()
-	for i in range(3):
-		t.forward(leng)
-		t.left(120)
 
 main()
